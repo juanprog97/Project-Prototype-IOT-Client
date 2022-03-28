6ac5701db8fd626f309e19111dd67ba008ca4b8f
juanprog97 <juancard190@gmail.com>
2022-03-28 18:43:11 -0500

    Added summary

--- src/views/ActivityToday.vue
+++ src/views/ActivityToday.vue
@@ -1,62 +1,83 @@
 <template>
-  <div>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porttitor diam
-    sit amet quam iaculis suscipit. Nulla porttitor arcu quis purus facilisis,
-    at consectetur ante pellentesque. Pellentesque et magna in dolor feugiat
-    tincidunt. Praesent volutpat elementum condimentum. Curabitur eleifend
-    elementum varius. Integer interdum elit vitae mollis ultrices. Fusce eu nisi
-    vitae velit faucibus tincidunt et in arcu. Nullam nec scelerisque elit, non
-    mattis tellus. Mauris luctus magna sed lectus consectetur laoreet. Sed ac
-    suscipit risus. Nam sit amet felis suscipit, aliquam diam vel, convallis
-    urna. Ut pharetra lectus ut fermentum fermentum. In auctor euismod eros, eu
-    vulputate mi consequat eu. Nullam pulvinar egestas commodo. Mauris id tempus
-    massa. Donec interdum orci in erat mollis mattis. Curabitur pharetra
-    scelerisque risus at scelerisque. Integer rhoncus neque quam, vel rutrum
-    ipsum ultrices id. Sed aliquet justo a lacus iaculis pellentesque.
-    Pellentesque consequat justo sed tempor accumsan. Mauris accumsan
-    pellentesque aliquet. Cras nisi elit, tristique ut neque sed, ultrices
-    congue orci. Pellentesque lectus massa, luctus et cursus vel, consequat quis
-    nisi. Sed id lacus sit amet orci venenatis ornare. Nullam gravida, ex a
-    tempus tincidunt, est est convallis risus, et efficitur leo dolor sed dui.
-    Suspendisse venenatis arcu tellus, et sollicitudin velit imperdiet ac. Duis
-    rutrum quam vitae eleifend elementum. Vivamus aliquam odio eget ante
-    scelerisque eleifend. Duis egestas dui vel mi imperdiet, non aliquet odio
-    fermentum. In hac habitasse platea dictumst. Integer in lectus enim. Integer
-    elementum sodales nunc nec dapibus. Nam eu leo efficitur, tempus nisl in,
-    interdum velit. Proin sed feugiat ipsum. Integer eu volutpat nibh, et
-    imperdiet neque. Duis et pellentesque nunc, eget cursus nisl. Vestibulum
-    ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-    Maecenas laoreet, urna eget sagittis gravida, nunc leo cursus nunc, nec
-    venenatis mi lorem a nisi. Curabitur vel ultrices ante. Class aptent taciti
-    sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-    Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus
-    justo neque, pharetra ac pulvinar non, tristique vel ante. Sed fermentum
-    convallis faucibus. Quisque eu ante et felis luctus porta at sed lorem. Nunc
-    id ligula a ante semper tincidunt convallis sed nunc. Aenean non felis
-    hendrerit, rutrum ipsum sit amet, vestibulum ante. Cras sapien eros,
-    vulputate accumsan ipsum vitae, tristique gravida odio. Sed faucibus, elit
-    vitae auctor condimentum, magna risus accumsan nisl, at consectetur sapien
-    lacus ut enim. Nulla et auctor lacus, nec bibendum leo. In id euismod sem,
-    sit amet pulvinar tellus. Nulla ultricies feugiat turpis vitae consequat.
-    Duis dictum nulla lectus, sed accumsan eros auctor eu. Cras bibendum
-    suscipit ipsum in posuere. Morbi pretium cursus justo a fringilla. Praesent
-    condimentum commodo nisi, a tempor sem rhoncus sed. Sed ultrices fringilla
-    diam, eu tristique diam placerat volutpat. Nulla fringilla, leo maximus
-    interdum vulputate, dui mi efficitur felis, vel euismod odio massa in nibh.
-    Sed felis nibh, rutrum a ornare non, auctor non sem. In ac pretium elit. Sed
-    volutpat posuere semper. Sed eu tempus sapien. Aenean libero orci, cursus in
-    diam ac, suscipit viverra orci. Maecenas nec cursus nibh. Nulla convallis,
-    neque eget malesuada ornare, nibh lacus porttitor augue, quis fringilla
-    lectus lacus ut lacus. Etiam molestie metus in iaculis auctor. Aenean
-    laoreet elit ante, pretium pretium risus lacinia in. Phasellus efficitur
-    pulvinar consectetur. Donec rutrum commodo erat, et placerat nunc accumsan
-    ac. Mauris gravida pharetra tellus, non rutrum dui rutrum at.
+  <div id="containerActivity">
+    <h2>Activity Today</h2>
+    <table>
+      <tr v-for="(device, index) in listDevices" v-bind:key="index">
+        <td>
+          <div class="containerdDeviceImage">
+            <ImageSVG :nameImage="device.imgSrc" width="50vw" />
+          </div>
+        </td>
+        <td>
+          <div id="details-device">
+            <p id="stateDevice">{{ device.state }} {{ device.name }}</p>
+            <p id="hourDevice">{{ device.time }}</p>
+          </div>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-export default {};
+import ImageSVG from "../components/FileSvg.vue";
+
+export default {
+  mounted() {
+    this.listDevices = this.$store.state.listDevices;
+  },
+  data() {
+    return {
+      listDevices: [],
+    };
+  },
+  components: {
+    ImageSVG,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+#containerActivity {
+  h2 {
+    text-align: center;
+  }
+
+  table {
+    width: auto;
+    margin: 2em 1em;
+    border-collapse: none;
+    border-spacing: 1em 1.2em;
+    tr {
+      td {
+        &:nth-child(1) {
+          .containerdDeviceImage {
+            display: flex;
+            justify-content: center;
+            border-radius: 100%;
+            background-color: #95cafe;
+            width: clamp(6em, 14vw, 16em);
+            height: clamp(6em, 14vw, 16em);
+          }
+        }
+        &:nth-child(2) {
+          #details-device {
+            display: block;
+
+            p {
+              &:nth-child(1) {
+                font-weight: bold;
+                font-size: clamp(1em, 1.4vw, 1.6em);
+                overflow-wrap: break-word;
+              }
+              &:nth-child(1) {
+                font-weight: bold;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
